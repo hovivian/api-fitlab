bdb0145aa853ea78fc0ffc8913c832b058172775
Vivian Ho <hovivian.wy@gmail.com>
2022-11-21 11:20:28 +0800

    fixed update profile api

--- src/controllers/api/my/profile/update.js
+++ src/controllers/api/my/profile/update.js
@@ -4,17 +4,7 @@ import prisma from '../../../_helpers/prisma.js'
 import handleErrors from '../../../_helpers/handle-errors.js'
 
 const updateSchema = yup.object({
-  email: yup.string().email().required().test({
-    message: () => 'Email already exists',
-    test: async (value) => {
-      try {
-        await prisma.user.findUnique({ where: { email: value }, rejectOnNotFound: true })
-        return false
-      } catch (err) {
-        return true
-      }
-    }
-  }),
+  email: yup.string().email().required(),
   username: yup.string().required().min(1, 'Minimum 6 characters').max(15, 'Maximum 15 characters'),
   height: yup.number().required(),
   targetWeight: yup.number().required()
@@ -24,7 +14,7 @@ const controllersApiMyProfileUpdate = async (req, res) => {
   try {
     const { body, session: { user: { id } } } = req
     const verifiedData = await updateSchema.validate(body, { abortEarly: false, stripUnknown: true })
-    const updated = await prisma.wishlist.update({ where: { id: Number(id) }, data: verifiedData })
+    const updated = await prisma.user.update({ where: { id: Number(id) }, data: verifiedData })
 
     return res.status(200).json(updated)
   } catch (err) {
